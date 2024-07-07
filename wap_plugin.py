63ecdbcf820c7a84574ee7c2d4ea57b3a6ff390a
akshay <akshay.dr.11@gmail.com>
2024-07-07 11:23:36 +0200

    fixed four indicators based on WaPORIPA Notebook

--- wap_plugin.py
+++ wap_plugin.py
@@ -1110,11 +1110,12 @@ class WAPlugin:
         param1_name = self.dlg.Param1ComboBox.currentText()
         param2_name = self.dlg.Param2ComboBox.currentText()
 
-        if self.indicator_key == 'Equity' or \
-           self.indicator_key == 'Relative Water Deficit':
+        if self.indicator_key == 'Uniformity of Water Consumption' or \
+            self.indicator_key == 'Adequacy (Relative Evapotranspiration)' or \
+            self.indicator_key == 'Relative Water Deficit':
             requirementsFlag = True if param1_name != '' else False
         if self.indicator_key == 'Beneficial Fraction' or \
-             self.indicator_key == 'Adequacy' or \
+             self.indicator_key == 'Beneficial Fraction New' or \
              self.indicator_key == 'Overall Consumed Ratio' or \
              self.indicator_key == 'Field Application Ratio (efficiency)' or \
              self.indicator_key == 'Depleted Fraction':
@@ -1135,24 +1136,23 @@ class WAPlugin:
         output_name = self.dlg.outputIndicName.text()+".tif"
         
         print(self.indicator_key)
-        if self.indicator_key == 'Equity':
+        if self.indicator_key == 'Uniformity of Water Consumption':
             self.indic_calc.equity(raster=param1_name, outLabel=self.dlg.outputIndicValue)
         elif self.indicator_key == 'Beneficial Fraction':
             self.indic_calc.beneficial_fraction(param1_name, param2_name, output_name)
             self.canv_manag.add_rast(output_name)
-        elif self.indicator_key == 'Adequacy':
-            try:
-                param3_name = float(self.dlg.Param3TextBox.text())
-            except ValueError:
-                print("Param 3 Input is not a float. Using Default value 1.25 instead")
-                self.dlg.Param3TextBox.setText('1.25')
-                param3_name = 1.25
-                
-            self.indic_calc.adequacy(param1_name, param2_name, output_name, Kc=param3_name)
+        elif self.indicator_key == 'Adequacy (Relative Evapotranspiration)':
+            # try:
+            #     param3_name = float(self.dlg.Param3TextBox.text())
+            # except ValueError:
+            #     print("Param 3 Input is not a float. Using Default value 1.25 instead")
+            #     self.dlg.Param3TextBox.setText('1.25')
+            #     param3_name = 1.25
+            self.indic_calc.adequacy(param1_name, output_name)
             self.canv_manag.add_rast(output_name)
-        # elif self.indicator_key == 'Relative Water Deficit':
-        #     self.indic_calc.relative_water_deficit(param1_name, output_name)
-        #     self.canv_manag.add_rast(output_name)
+        elif self.indicator_key == 'Relative Water Deficit':
+            self.indic_calc.relative_water_deficit(param1_name, output_name, outLabel=self.dlg.outputIndicValue)
+            self.canv_manag.add_rast(output_name)
         elif self.indicator_key == 'Overall Consumed Ratio':
             try:
                 param3_name = float(self.dlg.Param3TextBox.text())
